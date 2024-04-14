8624e788daa7206044fe5358de0f5e3c8038c7cd
KahlilMJPalisocMarcelino <bsukmm2026@gmail.com>
2024-04-14 21:31:09 +0400

    Petrol-Calcu-JAVA.js

---
+++ Chapter-6-Javascript/Exercises/Petrol-Calcu-JAVA.js
@@ -0,0 +1,15 @@
+
+function calculateTotalAmount() {
+    var costPerLiter = document.getElementById("costPerLiter").value;
+    var numberOfLiters = document.getElementById("numberOfLiters").value;
+
+    if (costPerLiter === "" || numberOfLiters === "") {
+        alert("You forgot to enter either the cost per liter and the number of liters.");
+        return;
+    }
+
+    var totalAmount = costPerLiter * numberOfLiters;
+
+    document.getElementById("totalCost").textContent = "Total Amount: AED " + totalAmount.toFixed(2);
+}
+
